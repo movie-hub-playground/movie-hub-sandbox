d50059ab21001a86f1fcdc9540f4c97a643cec15
Darío Román Jiménez <dariorojime@gmail.com>
2025-11-11 00:23:04 +0100

    fix: fakenodo y base_dataset refactorizado

--- app/modules/fakenodo/models.py
+++ app/modules/fakenodo/models.py
@@ -3,9 +3,9 @@ from app import db
 
 class Fakenodo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    movie_metadata = db.Column(db.JSON, nullable=False)
     status = db.Column(db.String(50), nullable=True, default="draft")
-    doi = db.Column(db.String(250), unique=True, nullable=True)
+    dataset_id = db.Column(db.Integer, db.ForeignKey("base_dataset.id"), nullable=False)
+    dataset = db.relationship("BaseDataset", back_populates="fakenodo", lazy=True)
 
     def __repr__(self):
         return f'Fakenodo<{self.id}>'
