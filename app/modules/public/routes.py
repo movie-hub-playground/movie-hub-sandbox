8b456e341625dd8a6e3a07691eb75bb7c3389715
Samuel Granado Oliva <samuelgranadooliva36@gmail.com>
2025-11-24 01:35:56 +0100

    fix: Ahora aparecen movie datasets en la pantalla principal
    Co-authored-by: Darío Román Jiménez  <dariorojime@gmail.com>

--- app/modules/public/routes.py
+++ app/modules/public/routes.py
@@ -3,6 +3,7 @@ import logging
 from flask import render_template
 
 from app.modules.dataset.services import DataSetService
+from app.modules.movie.services import MovieService
 from app.modules.featuremodel.services import FeatureModelService
 from app.modules.public import public_bp
 
@@ -13,10 +14,10 @@ logger = logging.getLogger(__name__)
 def index():
     logger.info("Access index")
     dataset_service = DataSetService()
+    movieDS_service = MovieService()
     feature_model_service = FeatureModelService()
 
     # Statistics: total datasets and feature models
-    datasets_counter = dataset_service.count_synchronized_datasets()
     feature_models_counter = feature_model_service.count_feature_models()
 
     # Statistics: total downloads
@@ -24,13 +25,13 @@ def index():
     total_feature_model_downloads = feature_model_service.total_feature_model_downloads()
 
     # Statistics: total views
-    total_dataset_views = dataset_service.total_dataset_views()
+    total_dataset_views = movieDS_service.total_dataset_views()
     total_feature_model_views = feature_model_service.total_feature_model_views()
 
     return render_template(
         "public/index.html",
-        datasets=dataset_service.latest_synchronized(),
-        datasets_counter=datasets_counter,
+        datasets=movieDS_service.get_all_moviedatasets(),
+        datasets_counter=len(movieDS_service.get_all_moviedatasets()),
         feature_models_counter=feature_models_counter,
         total_dataset_downloads=total_dataset_downloads,
         total_feature_model_downloads=total_feature_model_downloads,
